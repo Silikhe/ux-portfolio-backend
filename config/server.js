9b6897efbd57d24ec40c15708b45293ab2ccf5d0
Silikhe <silaswans@gmail.com>
2023-02-02 12:03:12 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e9757eb5160e0048a458939375e06fdd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9dd8296b-4cd2-4c50-a266-3ca4b2b80c10'
+};
